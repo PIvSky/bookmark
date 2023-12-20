ef8310fdd19a4bb22b2d77c51b8bf8528135007d
PIvSky <piotr.iwansky@gmail.com>
2023-12-20 13:28:18 +0100

    Completed Footer.js for Mobile and Deskopt

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,27 @@
 import '../styles/Footer.scss';
+import { navElements } from './Navbar';
+import { ReactComponent as BookmarkLogo} from '../assets/images/logo-bookmark.svg';
+import { ReactComponent as FbIcon } from '../assets/images/icon-facebook.svg';
+import { ReactComponent as TwIcon } from '../assets/images/icon-twitter.svg';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is the Footer</h1>
-        </div>
+        <footer className='footer'>
+            <div className='footer-title'>
+                <BookmarkLogo />
+            </div>
+            <ul className='footer-nav'>
+                {navElements.map((element, index) => (
+                    <li className='footer-nav__element' key={index}>
+                        {element}
+                    </li>
+                ))}
+            </ul>
+            <div className='footer-socialmedia'>
+                <FbIcon className='footer-socialmedia__item'/>
+                <TwIcon className='footer-socialmedia__item'/>
+            </div>
+        </footer>
     )
 }
 
