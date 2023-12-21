eab911ace5dd5c96c731b34ddeabf82b442fb4d7
PIvSky <piotr.iwansky@gmail.com>
2023-12-21 14:55:20 +0100

    Animated counting in Newsletter.js done

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,6 +1,18 @@
 import '../styles/Newsletter.scss';
 import Button from './Button';
 import '../styles/Button.scss';
+import { useSpring, animated } from 'react-spring';
+
+const AnimatedCounting = ({n}) => {
+    const { number } = useSpring({
+        from: {number: 35000},
+        number: n,
+        delay: 1000,
+        config: {mass: 1, tension: 20, friction: 10, duration: 20000},
+    });
+
+    return <animated.span>{number.to((n)=> n.toLocaleString('en-US', { maximumFractionDigits: 0 }))}</animated.span>
+}
 
 const Newsletter = () => {
 
@@ -15,7 +27,7 @@ const Newsletter = () => {
         <>
             <div className='newsletter'>
                 <div className='newsletter-container'>
-                    <p className='newsletter-container__joined'>35,000 + ALREADY JOINED</p>
+                    <p className='newsletter-container__joined'><AnimatedCounting n={0} /> + ALREADY JOINED</p>
                     <h2 className='newsletter-container__title'>Stay up-to-date with what we're doing</h2>
                     <div className='email-container'>
                         <input className='email-container__input' placeholder='Enter your email adress'></input>
